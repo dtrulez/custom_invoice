8c22f658b84f0cd31a39cdafa38715c7f0e0587e
dtrulez <yojtan@gmail.com>
2014-11-30 21:56:35 +0800

    initial commit

---
+++ __openerp__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Custom Invoice",
+    'version': "1.0",
+    'author': "YJ",
+    'category': "Tools",
+    'depends': ['account'],
+    'data': ['custom_invoice.xml'],
+    'demo': [],
+    'installable': True,
+}
+
---
+++ custom_invoice.py
@@ -0,0 +1,91 @@
+from openerp.osv import fields, osv
+from openerp import api
+from openerp.tools.translate import _
+
+class custom_invoice(osv.osv):
+    _name = "account.invoice"
+    _inherit = "account.invoice"
+
+    @api.multi
+    def onchange_partner_id(self, type, partner_id, date_invoice=False,
+            payment_term=False, partner_bank_id=False, company_id=False):
+        account_id = False
+        payment_term_id = False
+        fiscal_position = False
+        bank_id = False
+	pricelist = False
+
+        if partner_id:
+            p = self.env['res.partner'].browse(partner_id)
+            rec_account = p.property_account_receivable
+            pay_account = p.property_account_payable
+	    pricelist = p.property_product_pricelist and p.property_product_pricelist.id or False
+            if company_id:
+                if p.property_account_receivable.company_id and \
+                        p.property_account_receivable.company_id.id != company_id and \
+                        p.property_account_payable.company_id and \
+                        p.property_account_payable.company_id.id != company_id:
+                    prop = self.env['ir.property']
+                    rec_dom = [('name', '=', 'property_account_receivable'), ('company_id', '=', company_id)]
+                    pay_dom = [('name', '=', 'property_account_payable'), ('company_id', '=', company_id)]
+                    res_dom = [('res_id', '=', 'res.partner,%s' % partner_id)]
+                    rec_prop = prop.search(rec_dom + res_dom) or prop.search(rec_dom)
+                    pay_prop = prop.search(pay_dom + res_dom) or prop.search(pay_dom)
+                    rec_account = rec_prop.get_by_record(rec_prop)
+                    pay_account = pay_prop.get_by_record(pay_prop)
+                    if not rec_account and not pay_account:
+                        action = self.env.ref('account.action_account_config')
+                        msg = _('Cannot find a chart of accounts for this company, You should configure it. \nPlease go to Account Configuration.')
+                        raise RedirectWarning(msg, action.id, _('Go to the configuration panel'))
+
+            if type in ('out_invoice', 'out_refund'):
+                account_id = rec_account.id
+                payment_term_id = p.property_supplier_payment_term.id
+            fiscal_position = p.property_account_position.id
+            bank_id = p.bank_ids and p.bank_ids[0].id or False
+
+        result = {'value': {
+            'account_id': account_id,
+            'payment_term': payment_term_id,
+            'fiscal_position': fiscal_position,
+        }}
+
+        if type in ('in_invoice', 'in_refund'):
+            result['value']['partner_bank_id'] = bank_id
+
+        if payment_term != payment_term_id:
+            if payment_term_id:
+                to_update = self.onchange_payment_term_date_invoice(payment_term_id, date_invoice)
+                result['value'].update(to_update.get('value', {}))
+            else:
+                result['value']['date_due'] = False
+
+        if partner_bank_id != bank_id:
+            to_update = self.onchange_partner_bank(bank_id)
+            result['value'].update(to_update.get('value', {}))
+
+	if pricelist:
+	    result['value']['pricelist_id'] = pricelist
+
+        return result
+
+    def onchange_pricelist_id(self, cr, uid, ids, pricelist_id, invoice_lines, context=None):
+        context = context or {}
+        if not pricelist_id:
+            return {}
+        value = {
+            'currency_id': self.pool.get('product.pricelist').browse(cr, uid, pricelist_id, context=context).currency_id.id
+        }
+        if not invoice_lines or invoice_lines == [(6, 0, [])]:
+            return {'value': value}
+        warning = {
+            'title': _('Pricelist Warning!'),
+            'message' : _('If you change the pricelist of this order (and eventually the currency), prices of existing order lines will not be updated.')
+        }
+        return {'warning': warning, 'value': value}
+
+
+    _columns = {
+        'pricelist_id': fields.many2one('product.pricelist', 'Pricelist', required=True, readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}, help="Pricelist for current purchase order."),
+    }
+
