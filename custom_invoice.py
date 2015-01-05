079c21d2f3a54e416453a061b51b075cb56982e0
dtrulez <yojtan@gmail.com>
2015-01-06 00:10:27 +0800

    Updated pricelist field, and invoice line product_id_change

--- custom_invoice.py
+++ custom_invoice.py
@@ -1,76 +1,31 @@
 from openerp.osv import fields, osv
-from openerp import api
+from openerp import api, models
 from openerp.tools.translate import _
 
-class custom_invoice(osv.osv):
+class custom_invoice(models.Model):
     _name = "account.invoice"
     _inherit = "account.invoice"
 
     @api.multi
     def onchange_partner_id(self, type, partner_id, date_invoice=False,
             payment_term=False, partner_bank_id=False, company_id=False):
-        account_id = False
-        payment_term_id = False
-        fiscal_position = False
-        bank_id = False
+	res = super(custom_invoice, self).onchange_partner_id(type=type, partner_id=partner_id, date_invoice=date_invoice, payment_term=payment_term, partner_bank_id=partner_bank_id, company_id=company_id)
+
 	pricelist = False
 
-        if partner_id:
+	if partner_id:
             p = self.env['res.partner'].browse(partner_id)
-            rec_account = p.property_account_receivable
-            pay_account = p.property_account_payable
-	    pricelist = p.property_product_pricelist and p.property_product_pricelist.id or False
-            if company_id:
-                if p.property_account_receivable.company_id and \
-                        p.property_account_receivable.company_id.id != company_id and \
-                        p.property_account_payable.company_id and \
-                        p.property_account_payable.company_id.id != company_id:
-                    prop = self.env['ir.property']
-                    rec_dom = [('name', '=', 'property_account_receivable'), ('company_id', '=', company_id)]
-                    pay_dom = [('name', '=', 'property_account_payable'), ('company_id', '=', company_id)]
-                    res_dom = [('res_id', '=', 'res.partner,%s' % partner_id)]
-                    rec_prop = prop.search(rec_dom + res_dom) or prop.search(rec_dom)
-                    pay_prop = prop.search(pay_dom + res_dom) or prop.search(pay_dom)
-                    rec_account = rec_prop.get_by_record(rec_prop)
-                    pay_account = pay_prop.get_by_record(pay_prop)
-                    if not rec_account and not pay_account:
-                        action = self.env.ref('account.action_account_config')
-                        msg = _('Cannot find a chart of accounts for this company, You should configure it. \nPlease go to Account Configuration.')
-                        raise RedirectWarning(msg, action.id, _('Go to the configuration panel'))
-
-            if type in ('out_invoice', 'out_refund'):
-                account_id = rec_account.id
-                payment_term_id = p.property_supplier_payment_term.id
-            fiscal_position = p.property_account_position.id
-            bank_id = p.bank_ids and p.bank_ids[0].id or False
-
-        result = {'value': {
-            'account_id': account_id,
-            'payment_term': payment_term_id,
-            'fiscal_position': fiscal_position,
-        }}
-
-        if type in ('in_invoice', 'in_refund'):
-            result['value']['partner_bank_id'] = bank_id
-
-        if payment_term != payment_term_id:
-            if payment_term_id:
-                to_update = self.onchange_payment_term_date_invoice(payment_term_id, date_invoice)
-                result['value'].update(to_update.get('value', {}))
-            else:
-                result['value']['date_due'] = False
-
-        if partner_bank_id != bank_id:
-            to_update = self.onchange_partner_bank(bank_id)
-            result['value'].update(to_update.get('value', {}))
+            pricelist = p.property_product_pricelist and p.property_product_pricelist.id or False
 
 	if pricelist:
-	    result['value']['pricelist_id'] = pricelist
+	    res['value'].update({'pricelist_id': pricelist})
 
-        return result
+	return res
+
 
     def onchange_pricelist_id(self, cr, uid, ids, pricelist_id, invoice_lines, context=None):
         context = context or {}
+
         if not pricelist_id:
             return {}
         value = {
@@ -86,6 +41,50 @@ class custom_invoice(osv.osv):
 
 
     _columns = {
-        'pricelist_id': fields.many2one('product.pricelist', 'Pricelist', required=True, readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}, help="Pricelist for current purchase order."),
+        'pricelist_id': fields.many2one('product.pricelist', 'Pricelist', required=True, readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}, help="Pricelist for current customer invoice."),
     }
+
+
+class custom_invoice_line(models.Model):
+    _name = "account.invoice.line"
+    _inherit = "account.invoice.line"
+
+
+    @api.multi
+    def product_id_change(self, product, uom_id, qty=0, name='', type='out_invoice',
+            partner_id=False, fposition_id=False, price_unit=False, currency_id=False,
+            company_id=None):
+
+	res = super(custom_invoice_line, self).product_id_change(product=product, uom_id=uom_id, qty=qty, name=name, type=type, partner_id=partner_id, fposition_id=fposition_id, price_unit=price_unit, currency_id=currency_id, company_id=company_id)
+
+	warning_msgs = ''
+	context = self._context
+
+	pricelist = context.get('pricelist_id')
+        if not pricelist:
+            warn_msg = _('You have to select a pricelist or a customer in the sales form !\n'
+                    'Please set one before choosing a product.')
+            warning_msgs += _("No Pricelist ! : ") + warn_msg +"\n\n"
+        else:
+            price = self.pool.get('product.pricelist').price_get(self._cr, self._uid, [pricelist],
+                    product, qty or 1.0, partner_id)[pricelist]
+
+
+            if price is False:
+                warn_msg = _("Cannot find a pricelist line matching this product and quantity.\n"
+                        "You have to change either the product, the quantity or the pricelist.")
+
+                warning_msgs += _("No valid pricelist line found ! :") + warn_msg +"\n\n"
+            else:
+		res.get('value').update({'price_unit': price})
+                #result.update({'price_unit': price})
+        if warning_msgs:
+            warning = {
+                       'title': _('Configuration Error!'),
+                       'message' : warning_msgs
+		    }
+
+	return res
+
+
 
